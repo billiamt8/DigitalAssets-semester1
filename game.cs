457e927132e502b010f3103080ed2482521ed00f
billiamt8 <0408493@schoolsnet.act.edu.au>
2026-02-27 14:13:41 +1100

    Update game.cs

--- game.cs
+++ game.cs
@@ -2,6 +2,9 @@ using System;
 
 class GameSuite
 {
+	    static char[] board = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int choice;
+        static int flag = 0;
 	static void Main(string[] args)
 	{
 		bool running = true;
@@ -37,21 +40,21 @@ class GameSuite
 
 	static void PlayNC()
 	{	
+
 		bool runningNC = true;
 		while (runningNC) {
 		Console.WriteLine("---Naughts and Crosses---");	
-		string[] arr = {"1","2","3",
-						  "4","5","6",
-							"7","8","9"};
-			Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
-            Console.WriteLine("     |     |      ");
+		Console.WriteLine("Player 1 - X, Player 2 - O");
+
+			Console.WriteLine("	     |     |      ");
+            Console.WriteLine("	  {0}  |  {1}  |  {2}", board[1], board[2], board[3]);
+            Console.WriteLine("	_____|_____|_____ ");
+            Console.WriteLine("	     |     |      ");
+            Console.WriteLine("	  {0}  |  {1}  |  {2}", board[4], board[5], board[6]);
+            Console.WriteLine("	_____|_____|_____ ");
+            Console.WriteLine("	     |     |      ");
+            Console.WriteLine("	  {0}  |  {1}  |  {2}", board[7], board[8], board[9]);
+            Console.WriteLine("	     |     |      ");
 		
 				}
 		
@@ -60,8 +63,8 @@ class GameSuite
 	static Random rand = new Random();
 	static void PlayRPS()
 	{	Console.Clear();
-		bool runningRPS = true;
-		while (runningRPS) {
+		bool runningNC = true;
+		while (runningNC) {
 		Console.WriteLine("---Rock Paper Scissors---");	
 			
 					
@@ -70,7 +73,7 @@ class GameSuite
 	
 		while (true) {
 			int userInput;
-			Console.WriteLine("Press 1 for Rock, 2 for Paper, 3 for scissors and 4 to return");
+			Console.WriteLine("Press 1 for Rock, 2 for Paper, and 3 for scissors");
 			int.TryParse(Console.ReadLine(), out userInput);
 
 			Console.WriteLine($"The player chose {userInput}");
@@ -80,6 +83,7 @@ class GameSuite
 			if (computerInput == userInput) 
 			{
 				Console.WriteLine("It's a draw!");
+
 			}
 			else if (computerInput == 1 && userInput == 2) 
 			{
@@ -102,19 +106,75 @@ class GameSuite
 			} else if (computerInput == 3 && userInput == 2) 
 			{	
 				Console.WriteLine("You lose!");
-			}else 
-			{
+			} else {
 				Console.WriteLine("Invalid input, try again.");
 			}
 		}
+		}
 	}
-	
+	private static int CheckWin()
+        {
+            #region Horzontal Winning Condtion
+            //Winning Condition For First Row
+            if (board[1] == board[2] && board[2] == board[3])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Row
+            else if (board[4] == board[5] && board[5] == board[6])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Row
+            else if (board[6] == board[7] && board[7] == board[8])
+            {
+                return 1;
+            }
+            #endregion
+            #region vertical Winning Condtion
+            //Winning Condition For First Column
+            else if (board[1] == board[4] && board[4] == board[7])
+            {
+                return 1;
+            }
+            //Winning Condition For Second Column
+            else if (board[2] == board[5] && board[5] == board[8])
+            {
+                return 1;
+            }
+            //Winning Condition For Third Column
+            else if (board[3] == board[6] && board[6] == board[9])
+            {
+                return 1;
+            }
+            #endregion
+            #region Diagonal Winning Condition
+            else if (board[1] == board[5] && board[5] == board[9])
+            {
+                return 1;
+            }
+            else if (board[3] == board[5] && board[5] == board[7])
+            {
+                return 1;
+            }
+            #endregion
+            #region Checking For Draw
+            // If all the cells or values filled with X or O then any player has won the match
+            else if (board[1] != '1' && board[2] != '2' && board[3] != '3' && board[4] != '4' && board[5] != '5' && board[6] != '6' && board[7] != '7' && board[8] != '8' && board[9] != '9')
+            {
+                return -1;
+            }
+            #endregion
+            else
+            {
+                return 0;
+            }
+        }
+	 
 	static int randomNumberGenerator() {
 		int randomValue = rand.Next(1, 4);
 		return randomValue;
-	}
-}
-			
-				
-			
 		}
+	}
+
+
